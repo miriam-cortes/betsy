79692145b1b556e0260a42c3dbebd85b8beb0054
Nina Mutty <ninamutty@gmail.com>
2016-10-18 15:17:50 -0700

    products controller base methods (check with seed data)

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,5 +1,6 @@
 class ProductsController < ApplicationController
   before_action :find_product, only: [:show, :edit, :update]
+  before_action :find_merchant, only: [:new, :create, :edit, :update]
 
   def index
     @products = Product.all
@@ -9,15 +10,24 @@ class ProductsController < ApplicationController
   end
 
   def new
+    @product = Product.new
   end
 
   def create
+    @product = Product.new(product_params)
+    @product.merchant_id = @merchant.id
+    @product.save
   end
 
   def edit
   end
 
   def update
+    if @product.update(student_params)
+      redirect_to product_path
+    else
+      render :edit
+    end
   end
 
   def destroy
@@ -26,8 +36,15 @@ class ProductsController < ApplicationController
   private
 
   def find_product
-    @product = Product.find(params[:product_id])
+    @product = Product.find(params[:id])
   end
 
+  def find_merchant
+    @merchant = Merchant.find(params[:id])
+  end
+
+  def product_params
+    params.require(:product).permit(:name, :description, :price, :image, :inventory, :rating)
+  end
 
 end
