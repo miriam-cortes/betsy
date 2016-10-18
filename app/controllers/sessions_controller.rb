fe6812b040cd33891f809f98db363680e272d50c
Sabrina Wilbert <sabrinawilbert@gmail.com>
2016-10-18 16:25:51 -0700

    Added oAuth setup and sessions controller.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,7 +1,32 @@
 class SessionsController < ApplicationController
   def create
+    auth_hash = request.env['omniauth.auth']
+
+    flash[:notice] = "Login Failed!"
+    return redirect to root_path unless auth_hash['uid']
+
+    @merchant = Merchant.find_by(uid: auth_hash[:uid], provider: 'github')
+    if @merchant.nil?
+      # User doesn't match anything in the DB.
+      # Attempt to create a new user.
+      @merchant = Merchant.build_from_github(auth_hash)
+
+      flash[:notice] = "Unable to save the User"
+      return redirect_to root_path unless @merchant.save
+    end
+
+
+    #Save the user ID in the session
+    session[:user_id] = @merchant.id
+
+    flash[:notice] = "Successfully logged in!"
+    redirect_to root_path
   end
 
+
+
+
   def destroy
+    session[:user_id] = nil
   end
 end
