78ba4614027de5c4e1ef3f44d71ccb9ee9cb82a9
Olivia <reach.olivialegge@gmail.com>
2016-10-19 11:07:17 -0700

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,7 @@ Rails.application.routes.draw do
   resources :sessions
 
   get "/auth/:provider/callback" =>  "sessions#create"
->>>>>>> master
+
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
