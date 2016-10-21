38a78eafbfea26c323e2f706aabcd80c346c19c5
Olivia <reach.olivialegge@gmail.com>
2016-10-20 19:44:55 -0700

    category model validation test

--- test/models/category_test.rb
+++ test/models/category_test.rb
@@ -4,4 +4,10 @@ class CategoryTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+
+  test "should have the necessary required validators" do
+  category = Category.new
+  assert_not category.valid?
+  assert_equal [:name], category.errors.keys
+end
 end
