e193a8638ed36fc595da4a32895bca43e80b9b5c
Sabrina Wilbert <sabrinawilbert@gmail.com>
2016-10-21 09:37:52 -0700

    Moved methods into model

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,15 +3,30 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  helper_method :current_merchant, :current_guest
+  helper_method :current_merchant, :current_guest, :current_cart
 
   def current_merchant
     @current_merchant ||= Merchant.find_by(id: session[:merchant_id])
   end
 
   def current_guest
-    @current_guest ||= Guest.find_by(id: session[:user_id])
+    return @current_guest if @current_guest.present?
+    @current_guest = Guest.find_or_create_by(id: session[:user_id])
+      session[:user_id] = @current_guest.id
+    return @current_guest
   end
 
+  def current_cart
+    #if we are logged in as merchant
+    if current_merchant.present?
+      #get cart from current merchant
+      return current_merchant.cart
+    end
+    #if we are not a merchant
+    if current_guest.present?
+      #get cart from current guest cart
+    return current_guest.cart
+    end
+  end
 
 end
--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -4,22 +4,31 @@ class OrdersController < ApplicationController
   end
 
   def view_cart
-
-
   end
 
   def new
   end
 
   def create
+    if current_merchant.nil?
+      guest_create_cart
+    # elsif current_merchant.nil? && if @order.guest_id != nil
+    #   add_line_to_cart
+    # elsif current_merchant != nil
+      #other code to create cart or add line to cart.
+    # end
+    # end
+    end
   end
 
-def subtotal
-  @order.line_items do |line|
-  line_cost = @line.price * @line.qty
-  total_amount << line_cost
-  return total_amount  
-end
+  def subtotal
+    @order.line_items do |line|
+    line_cost = @line.price * @line.qty
+    total_amount << line_cost
+    return total_amount
+    end
+  end
+
     def total_amount
       subtotal
       total_amount.inject { |result, element| result + element }
@@ -38,36 +47,33 @@ end
   def destroy
   end
 
+
+
   def guest_create_cart
-    if current_merchant.nil? && if @order.guest_id.nil?
-    @guest = Guest.new
-    @guest.save
-    @order = Order.new
-    @order.guest_id = @guest.id
-    @order.order_status = "pending"
-    @order.save
+    @line = LineItem.new
+    @line.product_id = params["product_id"]
+    @line.merchant_id = params["merchant_id"]
+    @line.qty = params["qty"]
+    @line.price = params["price"]
+    @line.order_id = @order.id
+    @line.save
+  end
+  def add_line_to_cart
     @line = LineItem.new
     @line.order_id = @order.id
-    @line.product_id = params[:products][:product_id]
-    @line.merchant_id = params[:products][:merchant_id]
-    @line.qty = params[:qty]
-    @line.price = params[:products][:price]
+    @line.product_id = params[:product]["product_id"]
+    @line.merchant_id = params[:product]["merchant_id"]
+    @line.qty = params["qty"]
+    @line.price = params[:product]["price"]
     @line.save
-  else
-    add_to_cart
-    end
-  def add_to_cart
-    @line = LineItem.new
-    @line.order_id = @order.id
-    @line.product_id = params[:products][:product_id]
-    @line.merchant_id = params[:products][:merchant_id]
-    @line.qty = params[:qty]
-    @line.price = params[:products][:price]
-    @line.save
-    end
+  end
 
     def remove_from_cart
       @line = @order.line_items.find(params[:id])
         @line.id.destroy(params[:id].to_i)
     end
-end
+
+  
+
+
+end#end of class
