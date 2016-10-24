93f6681e2bd00d0891fd0771d7b6f55ce3087de8
Olivia <reach.olivialegge@gmail.com>
2016-10-24 12:36:48 -0700

    basic nav bar

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,7 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+  before_action  do define_category_variables(6) end
   helper_method :current_merchant, :current_guest
 
   def current_merchant
@@ -12,5 +13,11 @@ class ApplicationController < ActionController::Base
   def current_guest
     @current_guest ||= Guest.find_by(id: session[:user_id].to_i)
   end
+
+  def define_category_variables(limit)
+    @top_catgories = Category.order("lower(name) ASC").limit(limit)
+    @all_categories = Category.all
+  end
+
 
 end
