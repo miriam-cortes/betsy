a9f8a4e6aa02de82032e953091daa77926543184
Nina Mutty <ninamutty@gmail.com>
2016-10-25 11:21:36 -0700

    fixing paths in tests after nesting products in merchants

--- test/controllers/products_controller_test.rb
+++ test/controllers/products_controller_test.rb
@@ -2,18 +2,18 @@ require 'test_helper'
 
 class ProductsControllerTest < ActionController::TestCase
   test "should get index" do
-    get :index
+    get :index, {merchant_id: (products(:glitter).merchant).id}
     assert_response :success
     assert_template :index
   end
 
   test "should get show" do
-    get :show, {id: products(:glitter).id}
+    get :show, {merchant_id: (products(:glitter).merchant).id, id: products(:glitter).id}
     assert_response :success
   end
 
   test "should get new" do
-    get :new
+    get :new, {merchant_id: (products(:glitter).merchant).id}
     assert_response :success
     assert_template :new
   end
@@ -21,7 +21,7 @@ class ProductsControllerTest < ActionController::TestCase
   test "should be able to create a new product" do
     post_params = {product: {name: "Glitter Wand", description: "a glittery wand", price: 1500, image: 'http://placekitten.com/200/300', merchant_id: 2, inventory: 7, rating: 5}}
 
-    post :create, post_params
+    post :create, {merchant_id: (products(:glitter).merchant).id}, post_params
     assert_response :redirect
     # This action will need to change to merchant's create_new_product path
   end
@@ -29,28 +29,28 @@ class ProductsControllerTest < ActionController::TestCase
   test "creating a product changes the number of products" do
     assert_difference("Product.count",1) do
       post_params = {product: {name: "Glitter Wand", description: "a glittery wand", price: 1500, image: 'http://placekitten.com/200/300', merchant_id: 2, inventory: 7, rating: 5}}
-      post :create, post_params
+      post :create, {merchant_id: (products(:glitter).merchant).id}, post_params
     end
   end
 
   test "should get edit" do
-    get :edit, {id: products(:glitter).id}
+    get :edit, {merchant_id: (products(:glitter).merchant).id, id: products(:glitter).id}
     assert_response :success
   end
 
   test "should be able to update a product" do
-    put :update, {id: products(:glitter).id, product: {name: "Glitter Wand", description: "a glittery wand", price: 1500, image: 'http://placekitten.com/200/300', merchant_id: 2, inventory: 7, rating: 5}}
+    put :update, {merchant_id: (products(:glitter).merchant).id, id: products(:glitter).id, product: {name: "Glitter Wand", description: "a glittery wand", price: 1500, image: 'http://placekitten.com/200/300', merchant_id: 2, inventory: 7, rating: 5}}
     assert_redirected_to controller: "products", action: "show"
   end
 
   test "deleting a product changes the number of products" do
     assert_difference("Product.count", -1) do
-      delete :destroy, {id: products(:glitter).id }
+      delete :destroy, {merchant_id: (products(:glitter).merchant).id, id: products(:glitter).id }
     end
   end
 
   test "responds with error if file isn't found" do
-    delete :destroy, {id: 943}
+    delete :destroy, {merchant_id: (products(:glitter).merchant).id, id: 943}
     assert_response :missing
   end
 
