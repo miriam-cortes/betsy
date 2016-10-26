2be1f37021c854a7f2a1a8c950c1c9b97755f714
Nina Mutty <ninamutty@gmail.com>
2016-10-26 14:47:28 -0700

    creading landing index page

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
 
+  get 'landing/index'
+
   get "/auth/:provider/callback" =>  "sessions#create"
 
   # The priority is based upon order of creation: first created -> highest priority.
