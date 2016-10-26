34374107f5dcf7757f945a2a905187b72c97db8b
Miriam Cortes <mcortes.umiami@gmail.com>
2016-10-26 10:31:05 -0700

    coverage and tests for merchant model

--- test/models/merchant_test.rb
+++ test/models/merchant_test.rb
@@ -5,8 +5,44 @@ class MerchantTest < ActiveSupport::TestCase
     assert true
   end
 
-  # test "pending orders calculate as pending" do
-  #   assert_no_difference('Merchant.orders.revenue_paid_orders') do
-  # end
+  test "paid orders calculate as paid" do
+    assert_equal 500, merchants(:one).revenue_paid_orders
+  end
+
+  test "pending orders calculate correctly" do
+    assert_equal 600, merchants(:one).revenue_pending_orders
+  end
+
+  test "completed orders calculate correctly" do
+    assert_equal 300, merchants(:one).revenue_completed_orders
+  end
+
+  test "cancelled orders calculate correctly" do
+    assert_equal 300, merchants(:one).revenue_cancelled_orders
+  end
+
+  test "total orders calculate correctly" do
+    assert_equal 1400, merchants(:one).total_revenue
+  end
+
+  test "correct number of pending orders" do
+    assert_equal 2, merchants(:one).pending_orders
+  end
+
+  test "correct number of cancelled orders" do
+    assert_equal 1, merchants(:one).cancelled_orders
+  end
+
+  test "correct number of paid orders" do
+    assert_equal 2, merchants(:one).paid_orders
+  end
+
+  test "correct number of completed orders" do
+    assert_equal 1, merchants(:one).completed_orders
+  end
+
+  test "correct number of orders" do
+    assert_equal 5, merchants(:one).total_orders
+  end
 
 end
