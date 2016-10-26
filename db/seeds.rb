b54f8850679dee80c554a87650b323bf1867eb07
Sabrina Wilbert <sabrinawilbert@gmail.com>
2016-10-26 14:21:03 -0700

    Fixed merge conflict.

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,34 +8,34 @@
 
 require 'csv'
 
-# CSV.foreach('seeds_csvs/category.csv') do |csv_obj|
-#   Category.create(id: csv_obj[0].to_i, name: csv_obj[1])
-#  end
+CSV.foreach('seeds_csvs/category.csv') do |csv_obj|
+  Category.create(id: csv_obj[0].to_i, name: csv_obj[1])
+ end
 
-# CSV.foreach('seeds_csvs/guests.csv') do |csv_obj|
-#   Guest.create(id: csv_obj[0].to_i, name: csv_obj[1], email: csv_obj[2])
-# end
+CSV.foreach('seeds_csvs/guests.csv') do |csv_obj|
+  Guest.create(id: csv_obj[0].to_i, name: csv_obj[1], email: csv_obj[2])
+end
 
 CSV.foreach('seeds_csvs/merchant.csv') do |csv_obj|
   Merchant.create(id: csv_obj[0].to_i, name: csv_obj[1], email: csv_obj[2], password: csv_obj[3])
 end
 
-# CSV.foreach('seeds_csvs/orders.csv') do |csv_obj|
-#   Order.create(id: csv_obj[0].to_i, guest_id: csv_obj[1].to_i, total_amount: csv_obj[2].to_f/100, merchant_id: csv_obj[3].to_i, card_name: csv_obj[4], card_number: csv_obj[5], card_exp: csv_obj[6], card_cvv: csv_obj[7], billing_zip: csv_obj[8], shipping_street: csv_obj[9], shipping_city: csv_obj[10], shipping_state: csv_obj[11], shipping_zip: csv_obj[12],order_status: csv_obj[13])
-# end
+CSV.foreach('seeds_csvs/orders.csv') do |csv_obj|
+  Order.create(id: csv_obj[0].to_i, guest_id: csv_obj[1].to_i, total_amount: csv_obj[2].to_f/100, merchant_id: csv_obj[3].to_i, card_name: csv_obj[4], card_number: csv_obj[5], card_exp: csv_obj[6], card_cvv: csv_obj[7], billing_zip: csv_obj[8], shipping_street: csv_obj[9], shipping_city: csv_obj[10], shipping_state: csv_obj[11], shipping_zip: csv_obj[12],order_status: csv_obj[13])
+end
 
-# CSV.foreach('seeds_csvs/product_categories.csv') do |csv_obj|
-#   ProductCategory.create(id: csv_obj[0].to_i, category_id: csv_obj[1].to_i, product_id: csv_obj[2])
-# end
+CSV.foreach('seeds_csvs/product_categories.csv') do |csv_obj|
+  ProductCategory.create(id: csv_obj[0].to_i, category_id: csv_obj[1].to_i, product_id: csv_obj[2])
+end
 
-# CSV.foreach('seeds_csvs/products_orders.csv') do |csv_obj|
-#   ProductOrder.create(id: csv_obj[0].to_i, product_id: csv_obj[1].to_i, order_id: csv_obj[2].to_i)
-# end
+CSV.foreach('seeds_csvs/products_orders.csv') do |csv_obj|
+  ProductOrder.create(id: csv_obj[0].to_i, product_id: csv_obj[1].to_i, order_id: csv_obj[2].to_i)
+end
 
 CSV.foreach('seeds_csvs/products.csv') do |csv_obj|
   Product.create(id: csv_obj[0].to_i, name: csv_obj[1], description: csv_obj[2], price: csv_obj[3].to_i, image: csv_obj[4], inventory: csv_obj[5].to_i, merchant_id: csv_obj[6].to_i )
 end
 
-# CSV.foreach('seeds_csvs/reviews.csv') do |csv_obj|
-#   Review.create(id: csv_obj[0].to_i, product_id: csv_obj[1].to_i, description: csv_obj[2], author: csv_obj[3], stars: csv_obj[4].to_i)
-# end
+CSV.foreach('seeds_csvs/reviews.csv') do |csv_obj|
+  Review.create(id: csv_obj[0].to_i, product_id: csv_obj[1].to_i, description: csv_obj[2], author: csv_obj[3], stars: csv_obj[4].to_i)
+end
